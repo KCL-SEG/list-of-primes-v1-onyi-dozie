f7ab98239cf69b9f40d097ea6ad4e7c103c1631e
Onyi Dozie <onyiyechukwu.dozie@kcl.ac.uk>
2023-12-03 14:38:14 +0000

    solve prime problem

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+    number = 1
+    while(len(list) < number_of_primes):
+        number = number + 1
+        if(isAPrime(number)):
+            list.append(number)
     return list
+
+
+def isAPrime(x):
+    aPrime = True;
+    for i in range(2,x):
+        if(x % i ) == 0:
+            aPrime = False;
+    return aPrime
+        
+
+
